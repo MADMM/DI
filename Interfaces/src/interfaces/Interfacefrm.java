b779530e93862c1c42edadca05d5d7beae4f4754
MIGUEL ANGEL DEL MORAL MARTINEZ <miguelangeldelmoral@hotmail.com>
2023-10-29 18:02:55 +0100

    Add files via upload

---
+++ Interfaces/src/interfaces/Interfacefrm.java
@@ -0,0 +1,56 @@
+package interfaces;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class Interfacefrm {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Interfacefrm window = new Interfacefrm();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Interfacefrm() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(87, 73, 258, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(87, 121, 258, 20);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+	}
+}
